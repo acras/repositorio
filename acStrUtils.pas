16a570d3629b1a5976fc76eb836856eb16863080
acras <acras@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2006-01-24 13:33:20 +0000

    git-svn-id: http://svn.acras.com.br/repositorio@492 2579b3e0-257a-994a-8a5b-07e09b2024e3

--- acStrUtils.pas
+++ acStrUtils.pas
@@ -2,10 +2,22 @@ unit acStrUtils;
 
 interface
 
+
 uses
   classes, StrUtils, SysUtils;
 
+Type
+  TInfoPalavra = class
+    public
+      frequencia: integer;
+  end;
+
 function getStrField(str: string; delimiter: char; index: integer): string;
+function retiraEspacos(str: String): String;
+function RemoveAcento(str: String): String;
+function RemoveSimbolo(str: String): String;
+function extraiPalavras(texto: string): TStringList;
+function achaPalavra(texto, palavra: string): integer;
 
 implementation
 
@@ -24,5 +36,91 @@ begin
   end;
 end;
 
+function retiraEspacos(str: string): String;
+var
+  i: integer;
+  adicionouEspaco: boolean;
+begin
+  result := '';
+  adicionouEspaco := false;
+  for i := 1 to length(str) do
+  begin
+    if (str[i]<>' ') then
+    begin
+      result := result + str[i];
+      adicionouEspaco := False;
+    end
+    else
+    begin
+      if not adicionouEspaco then
+      begin
+        adicionouEspaco := true;
+        result := result + ' ';
+      end;
+    end;
+  end;
+end;
 
+function RemoveAcento(Str:String): String;
+Const
+  ComAcento = '‡‚ÍÙ˚„ı·ÈÌÛ˙Á¸‰¿¬ ‘€√’¡…Õ”⁄«‹ƒ';
+  SemAcento = 'aaeouaoaeioucuaAAEOUAOAEIOUCUA';
+Var
+  x : Integer;
+Begin
+  For x := 1 to Length(Str) do
+    if Pos(Str[x],ComAcento)<>0 Then
+      Str[x] := SemAcento[Pos(Str[x],ComAcento)];
+  Result := Str;
+end;
+
+function RemoveSimbolo(str: string): String;
+const
+  simbolo = '''"!@#$%.®&*()_-+=ß¨π¥`~^;:/\|<>][{}?!';
+var
+  i: integer;
+begin
+  for i:= 1 to length(str) do
+  begin
+     if Pos(str[i],simbolo)=0 Then
+      result := result + str[i];
+     if Pos(str[i],simbolo)<>0 Then
+       result := result + '';
+  end;
+end;
+
+function achaPalavra(texto, palavra: string): integer;
+begin
+  result := pos(palavra, texto);
+end;
+
+function extraiPalavras(texto: string): TStringList;
+var
+  strL: TStringList;
+  i, posicao: integer;
+  palavra: string;
+  info: TInfoPalavra;
+begin
+  palavra := '';
+  strL := TStringList.Create;
+  for i := 1 to length(texto) do
+  begin
+    //se È uma palavra nova e n„o foi inserida
+    if texto[i]=' ' then
+    begin
+      posicao := strL.add(palavra);
+      info := TInfoPalavra.Create;
+      info.frequencia := 1;
+      strL.Objects[posicao] := info;
+      palavra := '';
+    end
+    else
+      palavra := palavra+texto[i];
+   end;
+  //se sobrou uma ultima palavra adiciona
+  if palavra <> '' then
+    strL.add(palavra);
+  result := strL;
+end;
 end.
+
