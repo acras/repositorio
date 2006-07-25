8f3ec245577bb76e8a828768c74400992524e366
acras <acras@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2006-07-25 12:49:54 +0000

    git-svn-id: http://svn.acras.com.br/repositorio@1010 2579b3e0-257a-994a-8a5b-07e09b2024e3

--- CNAB400.pas
+++ CNAB400.pas
@@ -105,9 +105,34 @@ const
   FCarteira = '009';
 
   //constantes para os tipos de operação
-  toEntradaConfirmada = 2;
-  toLiquidacaoNormal  = 6;
-  toEntradaRejeitada  = 3;  
+  toEntradaConfirmada                       = 2;
+  toEntradaRejeitada                        = 3;
+  toLiquidacaoNormal                        = 6;
+  toBaixadoAutomaticamenteViaArquivo        = 9;
+  toBaixadoConformeInstrucaoAgencia         = 10;
+  toArquivoTitulosPendentes                 = 11;
+  toAbatimentoConncedido                    = 12;
+  toAbatimentoCancelado                     = 13;
+  toVencimentoAlterado                      = 14;
+  toLiquidacaoCartorio                      = 15;
+  toTituloPagoCheque                        = 16;
+  toLiquidacaoAposBaixaOuNaoRegistrado      = 17;
+  toAcertoDepositaria                       = 18;
+  toConfirmacaoRecebInstProtesto            = 19;
+  toConfirmacaoRecebInstSustacaoCheque      = 20;
+  toAcertoControleParticipante              = 21;
+  toTituloPagamentoCancelado                = 22;
+  toEntradaTituloCartorio                   = 23;
+  toEntradaRejeitadaCEPIrregular            = 24;
+  toBaixaRejeitada                          = 27;
+  toDebitoTarifasCustas                     = 28;
+  toAlteracaoOutrosDadosRejeitados          = 30;
+  toInstrucaoRejeitada                      = 32;
+  toConfirmacaoPedidoAlteracaoOutorsDados   = 33;
+  toRetiradoCartorioManutencaoCarteira      = 34;
+  toDesagendamentoDebitoAutomatico          = 35;
+  toAcertoDadosRateioCredito                = 68;
+  toCancelamentoDadosRateio                 = 69;
 
 implementation
 
@@ -257,6 +282,7 @@ begin
   with TStringField.Create(FClientDataSetRetorno) do
   begin
     FieldName := 'MotivoRecusa';
+    Size := 2000;
     DataSet   := FClientDataSetRetorno;
   end;
 
@@ -273,8 +299,8 @@ procedure TCNAB400.abrirArquivo(sNomeArquivo: string);
 var
   conteudo: TStringList;
   sHeader, linhaAtual: string;
-  iLinhaAtual, i, codMotivo: integer;
-  mensagem: string;
+  iLinhaAtual, i, codMotivo, tipoOperacao: integer;
+  mensagem, statusProtesto: string;
 begin
   conteudo := TStringList.Create;
   try
@@ -331,14 +357,314 @@ begin
             strToInt(copy(linhaAtual, 113, 2)),
             strToInt(copy(linhaAtual, 111, 2))
           );
+
+        tipoOperacao :=
+          FClientDataSetRetorno.FieldByName('TipoOcorrencia').AsInteger;
+
         mensagem := '';
-        for i := 0 to 4 do
+        case tipoOperacao of
+        //Entrada Confirmada
+        02 :
         begin
-          codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
-          if codMotivo <> 0 then
-          case codMotivo of
-            48: mensagem := mensagem + 'CEP Inválido. ';
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              00: mensagem := mensagem + 'Ocorrência aceita. ';
+              01: mensagem := mensagem + 'Código do Banco inválido.';
+              17: mensagem := mensagem + 'Data de vencimento anterior a data de emissão.';
+              21: mensagem := mensagem + 'Espécie do Título inválido.';
+              24: mensagem := mensagem + 'Data da emissão inválida.';
+              38: mensagem := mensagem + 'Prazo para protesto inválido.';
+              39: mensagem := mensagem + 'Pedido para protesto não permitido para título.';
+              43: mensagem := mensagem + 'Prazo para baixa e devolução inválido.';
+              45: mensagem := mensagem + 'Nome do Sacado inválido.';
+              46: mensagem := mensagem + 'Tipo/num. de inscrição do Sacado inválidos.';
+              47: mensagem := mensagem + 'Endereço do Sacado não informado.';
+              48: mensagem := mensagem + 'CEP irregular.';
+              50: mensagem := mensagem + 'CEP referente a Banco correspondente.';
+              53: mensagem := mensagem + 'Nº de inscrição do Sacador/avalista inválidos (CPF/CNPJ).';
+              54: mensagem := mensagem + 'Sacador/avalista não informado.';
+              67: mensagem := mensagem + 'Débito automático agendado';
+              68: mensagem := mensagem + 'Débito não agendado - erro nos dados de remessa.';
+              69: mensagem := mensagem + 'Débito não agendado - Sacado não consta no cadastro de autorizante.';
+              70: mensagem := mensagem + 'Débito não agendado - Cedente não autorizado pelo Sacado.';
+              71: mensagem := mensagem + 'Débito não agendado - Cedente não participa da modalidade de déb.automático.';
+              72: mensagem := mensagem + 'Débito não agendado - Código de moeda diferente de R$.';
+              73: mensagem := mensagem + 'Débito não agendado - Data de vencimento inválida.';
+              75: mensagem := mensagem + 'Débito não agendado - Tipo do número de inscrição do sacado debitado inválido.';
+              86: mensagem := mensagem + 'Seu número do documento inválido.';
+            end;
           end;
+        end;
+
+        //Entrada Rejeitada
+        03 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              02: mensagem := mensagem + 'Código do registro detalhe inválido.';
+              03: mensagem := mensagem + 'Código da ocorrência inválida.';
+              04: mensagem := mensagem + 'Código de ocorrência não permitida para a carteira.';
+              05: mensagem := mensagem + 'Código de ocorrência não numérico.';
+              07: mensagem := mensagem + 'Agência/conta/Digito - |Inválido.';
+              08: mensagem := mensagem + 'Nosso número inválido.';
+              09: mensagem := mensagem + 'Nosso número duplicado.';
+              10: mensagem := mensagem + 'Carteira inválida.';
+              16: mensagem := mensagem + 'Data de vencimento inválida.';
+              18: mensagem := mensagem + 'Vencimento fora do prazo de operação.';
+              20: mensagem := mensagem + 'Valor do Título inválido.';
+              21: mensagem := mensagem + 'Espécie do Título inválida.';
+              22: mensagem := mensagem + 'Espécie não permitida para a carteira.';
+              24: mensagem := mensagem + 'Data de emissão inválida.';
+              38: mensagem := mensagem + 'Prazo para protesto inválido.';
+              44: mensagem := mensagem + 'Agência Cedente não prevista.';
+              50: mensagem := mensagem + 'CEP irregular - Banco Correspondente.';
+              63: mensagem := mensagem + 'Entrada para Título já cadastrado.';
+              68: mensagem := mensagem + 'Débito não agendado - erro nos dados de remessa.';
+              69: mensagem := mensagem + 'Débito não agendado - Sacado não consta no cadastro de autorizante.';
+              70: mensagem := mensagem + 'Débito não agendado - Cedente não autorizado pelo Sacado.';
+              71: mensagem := mensagem + 'Débito não agendado - Cedente não participa do débito Automático.';
+              72: mensagem := mensagem + 'Débito não agendado - Código de moeda diferente de R$.';
+              73: mensagem := mensagem + 'Débito não agendado - Data de vencimento inválida.';
+              74: mensagem := mensagem + 'Débito não agendado - Conforme seu pedido, Título não registrado.';
+              75: mensagem := mensagem + 'Débito não agendado – Tipo de número de inscrição do debitado inválido.';
+            end;
+          end;
+        end;
+
+        //Liquidação
+        06 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              00: mensagem := mensagem + 'Título pago com dinheiro ( Novo ).';
+              15: mensagem := mensagem + 'Título pago com cheque.';
+            end;
+          end;
+        end;
+
+        //Baixado Automaticamente via arquivo
+        09 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              10: mensagem := mensagem + 'Baixa Comandada pelo Cliente. ';
+            end;
+          end;
+        end;
+
+        //Baixado pelo banco
+        10 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+
+              00: mensagem := mensagem + 'Baixado Conforme Instruções da Agência.';
+              14: mensagem := mensagem + 'Título Protestado.';
+              15: mensagem := mensagem + 'Título excluído.';
+              16: mensagem := mensagem + 'Título Baixado pelo Banco por decurso Prazo.';
+              20: mensagem := mensagem + 'Título Baixado e transferido para desconto.';
+            end;
+          end;
+        end;
+
+        //Liquidacao em cartório
+        15 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              00: mensagem := mensagem + 'Titulo pago com dinehiro. ';
+              15: mensagem := mensagem + 'Titulo pago com cheque. ';
+            end;
+          end;
+        end;
+
+        //Liquidação após baixa ou título não registrado
+        17 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              00: mensagem := mensagem + 'Título pago com dinheiro.';
+              15: mensagem := mensagem + 'Título pago com cheque.';
+            end;
+          end;
+        end;
+
+        //Confirmação de Recebimento Instrução de Protesto
+        19 :
+        begin
+            statusProtesto := copy(linhaAtual,295,1);
+            if statusProtesto <> '' then
+            if statusProtesto = 'A' then
+              mensagem := mensagem + 'Protesto aceito.';
+            if statusProtesto = 'D' then
+              mensagem := mensagem + 'Protesto desprezado.';
+        end;
+
+        //Entrada rejeitada por CEP irregular
+        24 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              48: mensagem := mensagem + 'CEP Inválido. ';
+            end;
+          end;
+        end;
+
+        //Baixa Rejeitada
+        27 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              04: mensagem := mensagem + 'Código de ocorrência não permitido para a carteira.';
+              07: mensagem := mensagem + 'Agência/Conta/dígito inválidos.';
+              08: mensagem := mensagem + 'Nosso número inválido.';
+              10: mensagem := mensagem + 'Carteira inválida.';
+              15: mensagem := mensagem + 'Carteira/Agência/Conta/nosso número inválidos.';
+              40: mensagem := mensagem + 'Título com ordem de protesto emitido.';
+              42: mensagem := mensagem + 'Código para baixa/devolução via Telebradesco inválido.';
+              60: mensagem := mensagem + 'Movimento para Título não cadastrado.';
+              77: mensagem := mensagem + 'Transferência para desconto não permitido para a carteira.';
+              85: mensagem := mensagem + 'Título com pagamento vinculado.';
+            end;
+          end;
+        end;
+
+        //Débito de tarifas/Custas
+        28 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              03: mensagem := mensagem + 'Tarifa de sustação.';
+              04: mensagem := mensagem + 'Tarifa de protesto.';
+              08: mensagem := mensagem + 'Custas de protesto.';
+            end;
+          end;
+        end;
+
+        //Alteração de outros dados rejeitados
+        30 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              01: mensagem := mensagem + 'Código do Banco inválido.';
+              04: mensagem := mensagem + 'Código de ocorrência não permitido para a carteira.';
+              05: mensagem := mensagem + 'Código da ocorrência não numérico.';
+              08: mensagem := mensagem + 'Nosso número inválido.';
+              15: mensagem := mensagem + 'Característica da cobrança imcopátivel.';
+              16: mensagem := mensagem + '.Data de vencimento inválido.';
+              17: mensagem := mensagem + 'Data de vencimento anterior a data de emissão.';
+              18: mensagem := mensagem + 'Vencimento fora do prazo de operação.';
+              24: mensagem := mensagem + 'Data de emissão Inválida.';
+              29: mensagem := mensagem + 'Valor do desconto maior/igual ao valor do Título.';
+              30: mensagem := mensagem + 'Desconto a conceder não confere.';
+              31: mensagem := mensagem + 'Concessão de desconto já existente ( Desconto anterior ).';
+              33: mensagem := mensagem + 'Valor do abatimento inválido.';
+              34: mensagem := mensagem + 'Valor do abatimento maior/igual ao valor do Título.';
+              38: mensagem := mensagem + 'Prazo para protesto inválido.';
+              39: mensagem := mensagem + 'Pedido de protesto não permitido para o Título.';
+              40: mensagem := mensagem + 'Título com ordem de protesto emitido.';
+              42: mensagem := mensagem + 'Código para baixa/devolução inválido.';
+              60: mensagem := mensagem + 'Movimento para Título não cadastrado.';
+              85: mensagem := mensagem + 'Título com Pagamento Vinculado.';
+            end;
+          end;
+        end;
+
+        //Instrução rejeitada
+        32 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              01: mensagem := mensagem + '.Código do Banco inválido.';
+              02: mensagem := mensagem + 'Código do registro detalhe inválido.';
+              04: mensagem := mensagem + 'Código de ocorrência não permitido para a carteira.';
+              05: mensagem := mensagem + 'Código de ocorrência não numérico.';
+              07: mensagem := mensagem + 'Agência/Conta/dígito inválidos.';
+              08: mensagem := mensagem + 'Nosso número inválido.';
+              10: mensagem := mensagem + 'Carteira inválida.';
+              15: mensagem := mensagem + 'Características da cobrança incompatíveis.';
+              16: mensagem := mensagem + 'Data de vencimento inválida.';
+              17: mensagem := mensagem + 'Data de vencimento anterior a data de emissão.';
+              18: mensagem := mensagem + 'Vencimento fora do prazo de operação.';
+              20: mensagem := mensagem + 'Valor do título inválido.';
+              21: mensagem := mensagem + 'Espécie do Título inválida.';
+              22: mensagem := mensagem + '.Espécie não permitida para a carteira.';
+              24: mensagem := mensagem + 'Data de emissão inválida.';
+              28: mensagem := mensagem + 'Código de desconto via Telebradesco inválido.';
+              29: mensagem := mensagem + 'Valor do desconto maior/igual ao valor do Título.';
+              30: mensagem := mensagem + 'Desconto a conceder não confere.';
+              31: mensagem := mensagem + 'Concessão de desconto - Já existe desconto anterior.';
+              33: mensagem := mensagem + 'Valor do abatimento inválido.';
+              34: mensagem := mensagem + 'Valor do abatimento maior/igual ao valor do Título.';
+              36: mensagem := mensagem + 'Concessão abatimento - Já existe abatimento anterior.';
+              38: mensagem := mensagem + 'Prazo para protesto inválido.';
+              39: mensagem := mensagem + 'Pedido de protesto não permitido para o Título.';
+              40: mensagem := mensagem + 'Título com ordem de protesto emitido.';
+              41: mensagem := mensagem + 'Pedido cancelamento/sustação para Título sem instrução de protesto.';
+              42: mensagem := mensagem + 'Código para baixa/devolução inválido.';
+              45: mensagem := mensagem + 'Nome do Sacado não informado.';
+              46: mensagem := mensagem + 'Tipo/número de inscrição do Sacado inválidos.';
+              47: mensagem := mensagem + 'Endereço do Sacado não informado.';
+              48: mensagem := mensagem + 'CEP Inválido.';
+              50: mensagem := mensagem + 'CEP referente a um Banco correspondente.';
+              53: mensagem := mensagem + 'Tipo de inscrição do sacador avalista inválidos.';
+              60: mensagem := mensagem + 'Movimento para Título não cadastrado.';
+              85: mensagem := mensagem + 'Título com pagamento vinculado.';
+              86: mensagem := mensagem + 'Seu número inválido.';
+            end;
+          end;
+        end;
+
+        //Desagendamento do débito automático
+        35 :
+        begin
+          for i := 0 to 4 do
+          begin
+            codMotivo := strToInt(copy(linhaAtual,319+i*2,2));
+            if codMotivo <> 0 then
+            case codMotivo of
+              81: mensagem := mensagem + 'Tentativas esgotadas, baixado.';
+              82: mensagem := mensagem + 'Tentativas esgotadas, pendente.';
+            end;
+          end;
+        end;
+
         end;
         FClientDataSetRetorno.fieldByName('MotivoRecusa').AsString :=
           mensagem;
