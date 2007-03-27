e469222b560eace5d6e6db6d41201dbf792598c5
acras <acras@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2007-03-27 17:19:57 +0000

    git-svn-id: http://svn.acras.com.br/repositorio@2685 2579b3e0-257a-994a-8a5b-07e09b2024e3

--- acStrUtils.pas
+++ acStrUtils.pas
@@ -33,7 +33,10 @@ begin
     str := '"' + AnsiReplaceStr(str, '|', '"|"') + '"';
     strL.Delimiter := delimiter;
     strL.DelimitedText := str;
-    result := strL[index-1];
+    if index>strL.Count then
+      result := ''
+    else
+      result := strL[index-1];
   finally
     FreeAndNil(strL);
   end;
