2b7e07fd6ab8795ab0c9d8519b8bf76915eba044
acras <acras@2579b3e0-257a-994a-8a5b-07e09b2024e3>
2012-06-13 13:43:30 +0000

    novas formas de calcular os hashes
    git-svn-id: http://svn.acras.com.br/repositorio@14889 2579b3e0-257a-994a-8a5b-07e09b2024e3

---
+++ acCryptoUtils.pas
@@ -0,0 +1,35 @@
+unit acCryptoUtils;
+
+interface
+
+uses IdHashMessageDigest, idHash, Classes, SysUtils;
+
+function MD5(const fileName : string) : string;
+function getMD5checksum(s: TStream): string;
+
+implementation
+
+function MD5(const fileName : string) : string;
+var
+  s: TFileStream;
+begin
+  s := TFileStream.Create(fileName, fmOpenRead);
+  try
+    result := getMD5checksum(s);
+  finally
+    FreeAndNil(s);
+  end;
+end;
+
+function getMD5checksum(s: TStream): string;
+var
+  md5: TIdHashMessageDigest5;
+  hash : T4x4LongWordRecord;
+begin
+  md5 := TIdHashMessageDigest5.Create;
+  s.Seek(0,0);
+  hash := md5.HashValue(s);
+  result := TIdHashMessageDigest5.AsHex(hash);
+end;
+
+end.
